c430544edaab4e3d07f4817177dd53dabd47a810
Michelle Totherow <tym4me13@gmail.com>
2024-02-23 19:46:41 -0600

    refactor attempting to adjust navbar placement in deployed site

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -32,50 +32,48 @@ export const NavBar = ({ token, setToken }) => {
       </div>
 
       <div className="navbar-menu w-full" ref={navbar}>
-        <div className="flex items-center justify-between">
-          <div className="navbar-start flex-grow">
-            {token && (
-              <>
-                <Link to="/" className="navbar-item text-blue-500 mr-4">
-                  <HomeIcon className="w-8 h-8 mr-2" />Home
-                </Link>
+        <div className="navbar-start">
+          {token && (
+            <>
+              <Link to="/" className="navbar-item text-blue-500 mr-4">
+                <HomeIcon className="w-8 h-8 mr-2" />Home
+              </Link>
 
-                <Link to="/postLists" className="navbar-item text-blue-500 mr-4">
-                  <DocumentTextIcon className="w-6 h-6 mr-2" /> Posts
+              <Link to="/postLists" className="navbar-item text-blue-500 mr-4">
+                <DocumentTextIcon className="w-6 h-6 mr-2" /> Posts
+              </Link>
+              <Link to="/myPosts" className="navbar-item text-blue-500 mr-4">
+                <UserCircleIcon className="w-6 h-6 mr-2" /> My Posts
+              </Link>
+              <Link to="/skills" className="navbar-item text-blue-500">
+                <CogIcon className="w-6 h-6 mr-2" /> Skill Manager
+              </Link>
+            </>
+          )}
+        </div>
+
+        <div className="navbar-end">
+          <div className="buttons">
+            {token ? (
+              <button
+                className="button is-outlined text-blue-500 mr-4"
+                onClick={() => {
+                  setToken("");
+                  navigate("/login");
+                }}
+              >
+                {"LOGOUT"}
+              </button>
+            ) : (
+              <>
+                <Link to="/register" className="button is-link text-white-500 mr-4">
+                  Register
                 </Link>
-                <Link to="/myPosts" className="navbar-item text-blue-500 mr-4">
-                  <UserCircleIcon className="w-6 h-6 mr-2" /> My Posts
-                </Link>
-                <Link to="/skills" className="navbar-item text-blue-500">
-                  <CogIcon className="w-6 h-6 mr-2" /> Skill Manager
+                <Link to="/login" className="button is-outlined text-blue-500">
+                  Login
                 </Link>
               </>
             )}
-          </div>
-
-          <div className="navbar-end">
-            <div className="buttons">
-              {token ? (
-                <button
-                  className="button is-outlined text-blue-500 mr-4"
-                  onClick={() => {
-                    setToken("");
-                    navigate("/login");
-                  }}
-                >
-                  {"LOGOUT"}
-                </button>
-              ) : (
-                <>
-                  <Link to="/register" className="button is-link text-white-500 mr-4">
-                    Register
-                  </Link>
-                  <Link to="/login" className="button is-outlined text-blue-500">
-                    Login
-                  </Link>
-                </>
-              )}
-            </div>
           </div>
         </div>
       </div>
